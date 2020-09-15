5f05aff3391b4a213829b1c6c7eb7e01fba457a7
LEESANGMIN <mytkdals93@gmail.com>
2020-09-15 20:05:23 +0900

    Return greetings for multiple people

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # gotutorial
 GO 공식 튜토리얼
+## 모듈 만들기
+- go mod init <경로지정>
+- path 안적으면 디렉토리 경로로 됨
+- go module은 디펜던시를 자동으로 입력시켜주는데 만약에 배포된 패키지가 아니라 로컬에 있는 패키지라면 replace를 지정하여 알려줘야함 hello/go.mod참고
+
+## 에러 핸들
+- GO에서는 함수에 에러 발생시 error를 리턴, 없으면 nil 리턴
+- 해당 함수를 콜한 곳에서 err처리
+- error.New("message") => error리턴. error.Error() => message리턴
+- greeting/greetings.go 참고
+
+## [log 패키지](https://golang.org/src/log/log.go?s=3306:3345#L60)
+- 로그 설정, prefix flag등
+- - log.SetPrefix("greeting: ")
+- -	log.SetFlags(log.LstdFlags | log.Lmsgprefix | log.Lshortfile)
+- - flag는  참조
+
+## [math/rand 패키지](https://golang.org/pkg/math/rand/)
+- 랜덤 숫자를 뽑을 때 이용.
+- - 시드 지정 rand.Seed(time.Now().UnixNano()) (프로그램 상 완전한 랜덤은 있을 수 없기 떄문에 타임을 이용해 늘 변경되는 값을 지정)
+- - 
--- hello/hello.go
+++ hello/hello.go
@@ -2,12 +2,45 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/mytkdals93/gotutorial/greetings"
 )
 
 func main() {
+
 	// Get a greeting message and print it.
-	message := greetings.Hello("Sangmin")
+	message, _ := greetings.Hello("Sangmin")
+
 	fmt.Println(message)
+	//executeGrettingWithError()
+	greetingMultipleNames()
+}
+func executeGrettingWithError() {
+	// Set properties of the predefined Logge, incliding
+	// the log entry prefix and a flag to disabe printing
+	// the time, source file, and line number.
+	log.SetPrefix("greeting.Hello: ")
+	log.SetFlags(0)
+
+	// Request a greeting message with empty string.
+	message, err := greetings.Hello("")
+	//If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+	// If no error was returend, print the returned message
+	// to the console
+	log.Println(message)
+}
+func greetingMultipleNames() {
+	log.SetPrefix("greeting.Hellos(): ")
+	log.SetFlags(0)
+	names := []string{"Gladys", "Samantha", "Darrin"}
+	g, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(g)
 }
